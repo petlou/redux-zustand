15346b90372c3fd29e94fe289d50d2d8d5bfa220
Peter Lourenço <lourenco.pet@gmail.com>
2024-09-30 21:31:06 -0300

    feat: Adição de hook para uso do react-query

---
+++ src/hooks/useProduct.tsx
@@ -0,0 +1,15 @@
+import { useInfiniteQuery } from '@tanstack/react-query'
+
+import { fetchProducts } from '../api/productApi'
+
+export function useProducts() {
+	return useInfiniteQuery({
+		queryKey: ['products'],
+		queryFn: fetchProducts,
+		initialPageParam: 1,
+		getNextPageParam: (lastPage, _pages) => {
+			return lastPage.nextPage <= lastPage.totalPages ? lastPage.nextPage : undefined
+		},
+		staleTime: 1000 * 60 * 5, // 5 minutos de validade dos dados
+	})
+}
