fa80617681fe54ae0c47ae5d07d37646898389ba
Peter Lourenço <lourenco.pet@gmail.com>
2024-09-30 21:30:06 -0300

    feat: Adição de react-query no app.tsx e adição de configurações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import ProductsList from './components/product'
 
 const queryClient = new QueryClient()
+
+const isDevelopment = import.meta.env.VITE_APP_ENV
 
 export function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<h1>Hello Peter!</h1>
+			<ProductsList />
+			{isDevelopment && <ReactQueryDevtools initialIsOpen={false} />}
 		</QueryClientProvider>
 	)
 }
