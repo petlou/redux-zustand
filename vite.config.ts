9cd291c342e21dc29b4223ca61a91ead7642648e
Peter Lourenço <lourenco.pet@gmail.com>
2024-10-01 11:25:37 -0300

    config: Adição de config para tests unitários

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/vitest-setup.ts',
+		exclude: [...configDefaults.exclude, 'e2e/**'],
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			reporter: ['html'],
+			all: true,
+			include: ['src/**/*.{ts,tsx,js,jsx}'],
+			reportsDirectory: './__test__/coverage',
+		},
+	},
 })
